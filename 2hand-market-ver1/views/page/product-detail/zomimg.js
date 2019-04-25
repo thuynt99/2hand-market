5366b0e134932a1fb10c630f58b770e075f4ed32
Nguyễn Thị Thúy <thuynguyen03091999@gmail.com>
2019-04-25 17:07:19 +0700

    My first commit

---
+++ 2hand-market-ver1/views/page/product-detail/zomimg.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+/*-- phóng to ảnh sản phẩm */
+      <script type="text/javascript">
+        $(document).ready(function() {
+          $('.show').zoomImage();
+          $(".imgProductSmall").click(function(){
+            let imgAppearence = $(this).attr("src");
+            $('.imgProduct').attr('src', imgAppearence)
+            $('#show-img').attr('src', $(this).attr('src'))
+            $('#big-img').attr('src', $(this).attr('src'))
+          });
+        });
+      </script>
+module.exports = router;
